de6cbe4d1abb05394fa5b53999d8cdcd68e78302
Sabareesh Kappagantu <sabareesh@sabareeshs-mbp.lan>
2017-03-29 15:20:04 -0700

    Initial Commit

---
+++ MovieScoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieScoop
+//
+//  Created by Sabareesh Kappagantu on 3/29/17.
+//  Copyright © 2017 Sabareesh Kappagantu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
